ffa7567f43a76d7e0b5ae804add291254661fbce
maraf <mara@neptuo.com>
2015-05-23 14:33:11 +0200

    Rewritten namespace remover service.

---
+++ src/Neptuo.Productivity.VisualStudio/FriendlyNamespaces/UnderscoreServiceActivator.cs
@@ -0,0 +1,29 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Neptuo.Activators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity.VisualStudio.FriendlyNamespaces
+{
+    public class UnderscoreServiceActivator : IActivator<UnderscoreService>
+    {
+        public UnderscoreService Create()
+        {
+            DTE dte = (DTE)ServiceProvider.GlobalProvider.GetService(typeof(DTE));
+            Ensure.NotNull(dte, "dte");
+
+            ProjectItemsEvents csharpProjectItemsEvents = (ProjectItemsEvents)dte.Events.GetObject("CSharpProjectItemsEvents");
+            Ensure.NotNull(csharpProjectItemsEvents, "csharpProjectItemsEvents");
+
+            OleMenuCommandService commandService = (OleMenuCommandService)ServiceProvider.GlobalProvider.GetService(typeof(IMenuCommandService));
+            Ensure.NotNull(commandService, "commandService");
+
+            return new UnderscoreService(dte, commandService, csharpProjectItemsEvents);
+        }
+    }
+}
--- src/Neptuo.Productivity.VisualStudio/_Services/IVsService.cs
+++ src/Neptuo.Productivity.VisualStudio/_Services/IVsService.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace Neptuo.Productivity.VisualStudio
 {
-    public interface IVsService
+    public interface IVsService : IDisposable
     {
-        void Stop();
     }
 }
