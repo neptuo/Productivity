8032e00624a47deef4c38740d57fe3b170750041
Marek Fišera <mara@neptuo.com>
2017-03-17 14:35:04 +0100

    Move duplication commands to edit menu.

--- src/Neptuo.Productivity.LineDuplication/VisualStudio/MyConstants.cs
+++ src/Neptuo.Productivity.LineDuplication/VisualStudio/MyConstants.cs
@@ -7,7 +7,7 @@ namespace Neptuo.Productivity.VisualStudio
     public static class MyConstants
     {
         public const string PackageString = "6b896283-2b15-4f82-bb4e-186c94784f79";
-        public const string CommandSetString = "700fe17e-577b-401a-8ae3-7dae52e38a3e";
+        public const string CommandSetString = "e4a2c479-8fe3-4778-9dca-6a6f3ef5948c";
         
         public static readonly Guid CommandSetGuid = new Guid(CommandSetString);
 
