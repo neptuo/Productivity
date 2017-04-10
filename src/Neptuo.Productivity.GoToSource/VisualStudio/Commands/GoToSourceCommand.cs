028bee9d50a50e73e349f0f45a7ec334cbf74d06
Marek Fišera <mara@neptuo.com>
2017-04-10 16:39:33 +0200

    Add menu command to the Edit menu, GoTo group.

---
+++ src/Neptuo.Productivity.GoToSource/VisualStudio/Commands/GoToSourceCommand.cs
@@ -0,0 +1,65 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity.VisualStudio.Commands
+{
+    /// <summary>
+    /// Binds 'Go To Source' commands.
+    /// </summary>
+    public class GoToSourceCommand
+    {
+        private VsPackage package;
+        private DTE dte;
+
+        public GoToSourceCommand(VsPackage package, DTE dte, IMenuCommandService commandService)
+        {
+            this.package = package;
+            this.dte = dte;
+
+            WireUpMenuCommands(commandService);
+        }
+
+        private void WireUpMenuCommands(IMenuCommandService commandService)
+        {
+            CommandID downCommandID = new CommandID(PackageGuids.CommandSet, PackageIds.GoToSource);
+            OleMenuCommand downCommand = new OleMenuCommand(OnGoToSource, downCommandID);
+            downCommand.BeforeQueryStatus += new EventHandler(OnBeforeQueryStatus);
+            commandService.AddCommand(downCommand);
+        }
+
+        private void OnBeforeQueryStatus(object sender, EventArgs e)
+        {
+            OleMenuCommand command = (OleMenuCommand)sender;
+            command.Enabled = true;
+        }
+
+        private void OnGoToSource(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Singleton
+
+        private static GoToSourceCommand instance;
+
+        /// <summary>
+        /// Initializes new (singleton) instance if not already created.
+        /// </summary>
+        /// <param name="package">An instance of the package.</param>
+        /// <param name="dte">A DTE.</param>
+        /// <param name="commandService">A menu command service.</param>
+        internal static void Initialize(VsPackage package, DTE dte, IMenuCommandService commandService)
+        {
+            if (instance == null)
+                instance = new GoToSourceCommand(package, dte, commandService);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Neptuo.Productivity.GoToSource/VisualStudio/VsPackage.cs
@@ -0,0 +1,31 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Neptuo.Productivity.VisualStudio.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity.VisualStudio
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration(ProductInfo.Name, ProductInfo.Description, VersionInfo.Version, IconResourceID = 400)]
+    [Guid(PackageGuids.PackageString)]
+    [ProvideAutoLoad(UIContextGuids80.NoSolution)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public class VsPackage : Package
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            DTE dte = (DTE)GetService(typeof(DTE));
+            IMenuCommandService commandService = (IMenuCommandService)GetService(typeof(IMenuCommandService));
+            GoToSourceCommand.Initialize(this, dte, commandService);
+        }
+    }
+}
