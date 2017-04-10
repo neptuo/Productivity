28940e225d13e170d7fce2f7abc8106dbad5db1a
Marek Fišera <mara@neptuo.com>
2017-04-10 16:16:21 +0200

    Add empty project for GoToSource.

---
+++ src/Neptuo.Productivity.GoToSource/Properties/ProductInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Neptuo.Productivity
+{
+    public static class ProductInfo
+    {
+        public const string Name = "Neptuo.Productivity.GoToSource";
+        public const string Description = "Productivity tools by Neptuo: Go to Source.";
+    }
+}
