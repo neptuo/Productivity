17fed03cb895bd296a5d59d1b5b86c23fa536624
Marek Fišera <mara@neptuo.com>
2017-04-10 16:55:52 +0200

    Basic implementation for navigating to ~/ paths.

--- src/Neptuo.Productivity.GoToSource/VisualStudio/Commands/GoToSourceCommand.cs
+++ src/Neptuo.Productivity.GoToSource/VisualStudio/Commands/GoToSourceCommand.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.Shell;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,7 +42,73 @@ namespace Neptuo.Productivity.VisualStudio.Commands
 
         private void OnGoToSource(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            if (dte.ActiveDocument != null)
+            {
+                TextDocument textDocument = dte.ActiveDocument.GetTextDocument();
+                if (textDocument != null)
+                {
+                    if (TryGetLineAt(textDocument, out string line, out int index))
+                    {
+                        string message = $"'{line}' at '{index}'";
+                        Console.WriteLine(message);
+
+                        if (TryGetStringLiteralAt(line, index, out string path))
+                        {
+                            message = $"Path is '{path}'";
+                            Console.WriteLine(path);
+
+                            TryOpenStringLiteral(path);
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool TryGetLineAt(TextDocument textDocument, out string content, out int index)
+        {
+            EditPoint currentPoint = textDocument.CreateEditPoint();
+
+            EditPoint startLine = textDocument.CreateEditPoint(textDocument.Selection.TopPoint);
+            startLine.StartOfLine();
+
+            // Create line end point.
+            EditPoint endLine = textDocument.CreateEditPoint(textDocument.Selection.TopPoint);
+            endLine.EndOfLine();
+
+            // Get line text content.
+            content = startLine.GetText(endLine);
+            index = textDocument.Selection.TopPoint.LineCharOffset;
+            return true;
+        }
+
+        private bool TryGetStringLiteralAt(string content, int index, out string path)
+        {
+            int indexOfStartQuote = content.IndexOf("\"", 0, index);
+            int indexOfEndQuote = content.IndexOf("\"", index);
+
+            if (indexOfStartQuote >= 0 && indexOfEndQuote >= 0)
+            {
+                indexOfStartQuote++;
+                path = content.Substring(indexOfStartQuote, indexOfEndQuote - indexOfStartQuote);
+                return true;
+            }
+
+            path = null;
+            return false;
+        }
+
+        private bool TryOpenStringLiteral(string literal)
+        {
+            string projectPath = Path.GetDirectoryName(dte.ActiveWindow.Project.FullName);
+            string filePath = literal.Replace("~/", projectPath + @"\");
+
+            ProjectItem item = dte.Solution.FindProjectItem(filePath);
+            if (item == null)
+                return false;
+
+            Window window = item.Open();
+            window.Activate();
+            return true;
         }
 
         #region Singleton
