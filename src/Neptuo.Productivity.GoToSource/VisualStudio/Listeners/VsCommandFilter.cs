8cdbccd47ff8b7a7359611ca06f9558d7b914035
Marek Fišera <mara@neptuo.com>
2017-04-10 18:08:52 +0200

    Add command filter for listening for F12 (GoToDefinition).

---
+++ src/Neptuo.Productivity.GoToSource/VisualStudio/Listeners/VsCommandFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Neptuo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity.VisualStudio.Listeners
+{
+    internal class VsCommandFilter : IOleCommandTarget
+    {
+        private readonly IOleCommandTarget nextController;
+        private readonly SVsServiceProvider serviceProvider;
+
+        public VsCommandFilter(IVsTextView textViewAdapter, SVsServiceProvider serviceProvider)
+        {
+            textViewAdapter.AddCommandFilter(this, out nextController);
+            this.serviceProvider = serviceProvider;
+        }
+
+        public int QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
+        {
+            return nextController.QueryStatus(ref pguidCmdGroup, cCmds, prgCmds, pCmdText);
+        }
+
+        public int Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+        {
+            if (nCmdID == (uint)VSConstants.VSStd97CmdID.GotoDefn)
+            {
+                Console.WriteLine("Go to...");
+            }
+            
+            int nextResult = nextController.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+            return nextResult;
+        }
+    }
+}
---
+++ src/Neptuo.Productivity.GoToSource/VisualStudio/Listeners/VsTextViewCreationListener.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.Editor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Shell;
+
+namespace Neptuo.Productivity.VisualStudio.Listeners
+{
+    /// <summary>
+    /// Text view creation listener for attaching F12 (GoToDefinition) commands.
+    /// </summary>
+    [Export(typeof(IVsTextViewCreationListener))]
+    [Name("GoToSource Command Listener.")]
+    [ContentType("code")]
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
+    internal class VsTextViewCreationListener : IVsTextViewCreationListener
+    {
+        [Import]
+        internal IVsEditorAdaptersFactoryService AdapterService { get; set; }
+
+        [Import]
+        internal SVsServiceProvider ServiceProvider { get; set; }
+
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            ITextView textView = AdapterService.GetWpfTextView(textViewAdapter);
+            textView.Properties.GetOrCreateSingletonProperty(() => new VsCommandFilter(textViewAdapter, ServiceProvider));
+        }
+    }
+}
