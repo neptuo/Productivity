8be95564a0294e7ee5c7493eb2e8377ebbe9e2e4
Marek Fišera <mara@neptuo.com>
2017-04-12 17:26:57 +0200

    Add service for running GoToSource.

---
+++ src/Neptuo.Productivity.GoToSource/GoToSourceService.cs
@@ -0,0 +1,49 @@
+﻿using Neptuo.Productivity.Parsers;
+using Neptuo.Productivity.Processors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity
+{
+    [Export(typeof(GoToSourceService))]
+    public class GoToSourceService
+    {
+        [ImportMany]
+        public IEnumerable<IPathParser> Parsers { get; set; }
+
+        [ImportMany]
+        public IEnumerable<IPathProcessor> Processors { get; set; }
+
+        public bool TryRun(string line, int index)
+        {
+            return TryParse(line, index, out string path) && TryRun(path);
+        }
+
+        private bool TryParse(string line, int index, out string path)
+        {
+            foreach (IPathParser parser in Parsers)
+            {
+                if (parser.TryParse(line, index, out path))
+                    return true;
+            }
+
+            path = null;
+            return false;
+        }
+
+        private bool TryRun(string path)
+        {
+            foreach (IPathProcessor processor in Processors)
+            {
+                if (processor.TryRun(path))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
