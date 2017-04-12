d085b0a0fdf83bea31a894c0afc048bdd1083445
Marek Fišera <mara@neptuo.com>
2017-04-12 21:40:25 +0200

    Add test project for GoToSource.

---
+++ test/GoToSource/GoToSource/CSharp/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoToSource.CSharp
+{
+    class Class1
+    {
+        public const string Path = "~/Json/JsonFile1.json";
+    }
+}
---
+++ test/GoToSource/GoToSource/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoToSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Path is " + @"CSharp\Class1.cs");
+        }
+    }
+}
