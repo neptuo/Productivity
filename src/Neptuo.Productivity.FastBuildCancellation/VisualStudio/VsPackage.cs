4841b1ebd017b938a6236482c8d57a135cd1ad79
Marek Fišera <mara@neptuo.com>
2017-05-08 21:25:03 +0200

    Cancelling build on first error.

--- src/Neptuo.Productivity.FastBuildCancellation/VisualStudio/VsPackage.cs
+++ src/Neptuo.Productivity.FastBuildCancellation/VisualStudio/VsPackage.cs
@@ -16,9 +16,31 @@ namespace Neptuo.Productivity.VisualStudio
     [ProvideAutoLoad(UIContextGuids80.NoSolution)]
     public class VsPackage : Package
     {
+        private DTE dte;
+        private BuildEvents events;
+
         protected override void Initialize()
         {
             base.Initialize();
+
+            dte = (DTE)GetService(typeof(DTE));
+            events = dte.Events.BuildEvents;
+
+            events.OnBuildProjConfigDone += OnProjectBuildCompleted;
+        }
+
+        private void OnProjectBuildCompleted(string project, string projectConfig, string platform, string solutionConfig, bool isSuccess)
+        {
+            if (!isSuccess)
+                dte.ExecuteCommand("Build.Cancel");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (disposing)
+                events.OnBuildProjConfigDone -= OnProjectBuildCompleted;
         }
     }
 }
