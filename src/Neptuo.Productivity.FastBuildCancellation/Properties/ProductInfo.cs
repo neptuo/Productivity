85b0771d26e85978aaabee5bdae125d7d0fc81e2
Marek Fišera <mara@neptuo.com>
2017-05-08 21:20:10 +0200

    Add empty project.
    # Conflicts:
    #	Neptuo.Productivity.sln

---
+++ src/Neptuo.Productivity.FastBuildCancellation/Properties/ProductInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Neptuo.Productivity
+{
+    public static class ProductInfo
+    {
+        public const string Name = "Neptuo.Productivity.FastBuildCancellation";
+        public const string Description = "Productivity tools by Neptuo: Build cancellation on first error.";
+    }
+}
---
+++ src/Neptuo.Productivity.FastBuildCancellation/VisualStudio/VsPackage.cs
@@ -0,0 +1,24 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity.VisualStudio
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration(ProductInfo.Name, ProductInfo.Description, VersionInfo.Version, IconResourceID = 400)]
+    [Guid(PackageGuids.PackageString)]
+    [ProvideAutoLoad(UIContextGuids80.NoSolution)]
+    public class VsPackage : Package
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
