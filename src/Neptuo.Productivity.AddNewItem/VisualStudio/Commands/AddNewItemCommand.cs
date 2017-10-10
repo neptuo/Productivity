338d6938a9592df2e281d44db117182dd06015dd
Marek Fišera <mara@neptuo.com>
2017-10-10 16:57:52 +0200

    Add command to context menu.

---
+++ src/Neptuo.Productivity.AddNewItem/VisualStudio/Commands/AddNewItemCommand.cs
@@ -0,0 +1,50 @@
+﻿using EnvDTE;
+using System;
+using System.ComponentModel.Design;
+
+namespace Neptuo.Productivity.VisualStudio.Commands
+{
+    /// <summary>
+    /// Binds 'Add New Item' commands.
+    /// </summary>
+    internal sealed class AddNewItemCommand
+    {
+        private DTE dte;
+
+        private AddNewItemCommand(DTE dte, IMenuCommandService commandService)
+        {
+            this.dte = dte;
+
+            WireUpMenuCommands(commandService);
+        }
+
+        private void WireUpMenuCommands(IMenuCommandService commandService)
+        {
+            CommandID commandId = new CommandID(PackageGuids.CommandSet, PackageIds.AddNewItem);
+            MenuCommand command = new MenuCommand(OnExecute, commandId);
+            commandService.AddCommand(command);
+        }
+
+        private void OnExecute(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Add New Item...");
+        }
+
+        #region Singleton
+
+        private static AddNewItemCommand instance;
+
+        /// <summary>
+        /// Initializes new (singleton) instance if not already created.
+        /// </summary>
+        /// <param name="dte">A DTE.</param>
+        /// <param name="commandService">A menu command service.</param>
+        internal static void Initialize(DTE dte, IMenuCommandService commandService)
+        {
+            if (instance == null)
+                instance = new AddNewItemCommand(dte, commandService);
+        }
+
+        #endregion
+    }
+}
