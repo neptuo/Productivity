a2f3717cdb46021fe40b6d4659b7eca8fe1f3a8c
Marek Fišera <mara@neptuo.com>
2017-10-10 20:58:41 +0200

    Hide window on Esc.

--- src/Neptuo.Productivity.AddNewItem/VisualStudio/Views/AddNewItemWindow.cs
+++ src/Neptuo.Productivity.AddNewItem/VisualStudio/Views/AddNewItemWindow.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 using Neptuo.Productivity.VisualStudio.Views.DesignData;
 using System;
 using System.Collections.Generic;
@@ -8,12 +9,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows.Media;
 
 namespace Neptuo.Productivity.VisualStudio.Views
 {
     [Guid("088f3f0e-a9e1-4e31-b77b-69e6d0fa5294")]
     public class AddNewItemWindow : ToolWindowPane
     {
+        private const int VK_ESCAPE = 0x1b;
+        private const int WM_KEYDOWN = 0x100;
+
+        private IntPtr handle;
+
         public AddNewItemWindow()
         {
             Caption = "Add new item...";
@@ -21,6 +30,30 @@ namespace Neptuo.Productivity.VisualStudio.Views
             {
                 DataContext = ViewModelLocator.Main
             };
+        }
+
+        private void EnsureHandle()
+        {
+            if (handle == IntPtr.Zero)
+            {
+                HwndSource source = (HwndSource)PresentationSource.FromVisual((Visual)Content);
+                if (source != null)
+                    handle = source.Handle;
+            }
+        }
+
+        protected override bool PreProcessMessage(ref Message m)
+        {
+            EnsureHandle();
+            if (m.HWnd == handle && m.Msg == WM_KEYDOWN && m.WParam.ToInt32() == VK_ESCAPE)
+                Hide();
+
+            return base.PreProcessMessage(ref m);
+        }
+
+        public void Hide()
+        {
+            ((IVsWindowFrame)Frame).Hide();
         }
     }
 }
