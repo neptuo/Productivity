90b7ff4db310234b9f5d3344f3171b202679c81c
Marek Fišera <mara@neptuo.com>
2017-10-10 16:49:19 +0200

    Add empty project.

---
+++ src/Neptuo.Productivity.AddNewItem/Properties/ProductInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Neptuo.Productivity
+{
+    public static class ProductInfo
+    {
+        public const string Name = "Neptuo.Productivity.AddNewItem";
+        public const string Description = "Productivity tools by Neptuo: Easily create any type of file or folder and include it in project.";
+    }
+}
