3e945051ebdfbf5f7b058cd557a9dde175965dcd
Marek Fišera <mara@neptuo.com>
2017-10-12 21:00:12 +0200

    Basic file template structure.

---
+++ src/Neptuo.Productivity.AddNewItem/FileTemplate.cs
@@ -0,0 +1,38 @@
+﻿using Neptuo;
+using Neptuo.Collections.Specialized;
+using Neptuo.FileSystems;
+using Neptuo.Productivity.VisualStudio.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity
+{
+    public class FileTemplate : ITemplate
+    {
+        private readonly string filePath;
+        private readonly string content;
+
+        public Encoding Encoding { get; }
+
+        public FileTemplate(string filePath)
+        {
+            Ensure.Condition.FileExists(filePath, "filePath");
+            this.filePath = filePath;
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                content = reader.ReadToEnd();
+                Encoding = reader.CurrentEncoding;
+            }
+        }
+
+        public TemplateContent GetContent(IReadOnlyKeyValueCollection parameters)
+        {
+            return new TemplateContent(content);
+        }
+    }
+}
--- src/Neptuo.Productivity.AddNewItem/VisualStudio/ViewModels/TemplateContent.cs
+++ src/Neptuo.Productivity.AddNewItem/VisualStudio/ViewModels/TemplateContent.cs
@@ -11,6 +11,10 @@ namespace Neptuo.Productivity.VisualStudio.ViewModels
         public string Content { get; private set; }
         public int Position { get; private set; }
 
+        public TemplateContent(string content)
+            : this(content, 0)
+        { }
+
         public TemplateContent(string content, int position)
         {
             Content = content;
