25393ca4904e69b6e7705c0d5726bfc70a2450cd
Marek Fišera <mara@neptuo.com>
2017-10-16 19:24:09 +0200

    Using content and file templates.

--- src/Neptuo.Productivity.AddNewItem/XmlTemplateService.cs
+++ src/Neptuo.Productivity.AddNewItem/XmlTemplateService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Xml.Serialization;
@@ -13,11 +14,14 @@ namespace Neptuo.Productivity
 {
     public class XmlTemplateService : ITemplateService
     {
+        private readonly string directoryPath;
         private readonly TemplateList list;
 
         public XmlTemplateService(string sourcePath)
         {
             Ensure.Condition.FileExists(sourcePath, "sourcePath");
+
+            directoryPath = Path.GetDirectoryName(sourcePath);
 
             XmlSerializer serializer = new XmlSerializer(typeof(TemplateList));
             using (FileStream sourceContent = new FileStream(sourcePath, FileMode.Open))
@@ -26,8 +30,31 @@ namespace Neptuo.Productivity
 
         public ITemplate FindTemplate(string path)
         {
-            throw new NotImplementedException();
+            string fileName = Path.GetFileName(path);
+            foreach (TemplateNode templateNode in list)
+            {
+                foreach (SelectorNode selectorNode in templateNode.Selector)
+                {
+                    if (selectorNode.IsMatched(fileName))
+                        return CreateTemplate(templateNode);
+                }
+            }
+
+            return null;
         }
+
+        private ITemplate CreateTemplate(TemplateNode node)
+        {
+            if (node.File != null)
+                return new FileTemplate(Path.Combine(directoryPath, node.File.Path));
+
+            if (node.Content != null)
+                return new StringTemplate(node.Content);
+
+            return null;
+        }
+
+        #region Xml Nodes
 
         [XmlRoot("Templates")]
         public class TemplateList : List<TemplateNode>
@@ -55,8 +82,18 @@ namespace Neptuo.Productivity
 
         public class SelectorNode
         {
+            private Regex regex;
+
             [XmlAttribute]
             public string FileName { get; set; }
+
+            public bool IsMatched(string fileName)
+            {
+                if (regex == null)
+                    regex = new Regex("^" + FileName.Replace("*", "(.*)") + "$");
+
+                return regex.IsMatch(fileName);
+            }
         }
 
         public class SnippetNode
@@ -77,5 +114,7 @@ namespace Neptuo.Productivity
         public class VsTemplateNode
         {
         }
+
+        #endregion
     }
 }
