8c45d7ae81453837938017f9520db7630b75ca31
Marek Fišera <mara@neptuo.com>
2019-04-17 20:44:23 +0200

    #27 - Migrate to AsyncPackage.

--- src/Neptuo.Productivity.LineDuplication/VisualStudio/VsPackage.cs
+++ src/Neptuo.Productivity.LineDuplication/VisualStudio/VsPackage.cs
@@ -1,26 +1,36 @@
 ﻿using EnvDTE;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Neptuo.Productivity.VisualStudio.Commands;
 using System;
 using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
 
 namespace Neptuo.Productivity.VisualStudio
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration(ProductInfo.Name, ProductInfo.Description, VersionInfo.Version, IconResourceID = 400)]
     [Guid(PackageGuids.PackageString)]
-    [ProvideAutoLoad(UIContextGuids80.NoSolution)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    public sealed partial class VsPackage : Package
+    public sealed partial class VsPackage : AsyncPackage
     {
-        protected override void Initialize()
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            base.Initialize();
+            await base.InitializeAsync(cancellationToken, progress);
+            await InitializeCommandsAsync(cancellationToken);
+        }
 
-            DTE dte = (DTE)GetService(typeof(DTE));
-            IMenuCommandService commandService = (IMenuCommandService)GetService(typeof(IMenuCommandService));
+        private async Task InitializeCommandsAsync(CancellationToken cancellationToken)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            DTE dte = (DTE)await GetServiceAsync(typeof(DTE));
+            IMenuCommandService commandService = (IMenuCommandService)await GetServiceAsync(typeof(IMenuCommandService));
             LineDuplicationCommand.Initialize(this, dte, commandService);
         }
     }
