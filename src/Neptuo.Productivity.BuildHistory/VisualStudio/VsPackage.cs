af97441a4619700e58f7a65d839e1c11b9ed8421
Marek Fišera <mara@neptuo.com>
2019-04-20 09:36:59 +0200

    #31 - Migrate to AsyncPackage.

--- src/Neptuo.Productivity.BuildHistory/VisualStudio/VsPackage.cs
+++ src/Neptuo.Productivity.BuildHistory/VisualStudio/VsPackage.cs
@@ -1,6 +1,6 @@
 ﻿using EnvDTE;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 using Neptuo.Events;
 using Neptuo.Productivity.UI.ViewModels;
 using Neptuo.Productivity.UI.Views;
@@ -8,33 +8,47 @@ using Neptuo.Productivity.VisualStudio.Commands;
 using Neptuo.Productivity.VisualStudio.Options;
 using Neptuo.Productivity.VisualStudio.UI;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
 
 namespace Neptuo.Productivity.VisualStudio
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration(ProductInfo.Name, ProductInfo.Description, VersionInfo.Version, IconResourceID = 400)]
     [Guid(PackageGuids.PackageString)]
-    [ProvideAutoLoad(UIContextGuids80.NoSolution)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideToolWindow(typeof(QuickWindow))]
     [ProvideOptionPage(typeof(ConfigurationPage), "Neptuo Productivity", "Build History", 0, 0, true)]
-    public sealed partial class VsPackage : Package
+    public sealed partial class VsPackage : AsyncPackage
     {
-        protected override void Initialize()
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            base.Initialize();
+            await base.InitializeAsync(cancellationToken, progress);
+            await InitializeCommandsAsync(cancellationToken);
+        }
+
+        private async Task InitializeCommandsAsync(CancellationToken cancellationToken)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            DTE dte = await GetServiceAsync<DTE>();
+            IMenuCommandService commandService = await GetServiceAsync<IMenuCommandService>();
 
             DefaultEventManager eventManager = new DefaultEventManager();
-            DTE dte = (DTE)GetService(typeof(DTE));
-            IMenuCommandService commandService = (IMenuCommandService)GetService(typeof(IMenuCommandService));
-
             ServiceFactory.EventHandlers = eventManager;
             ServiceFactory.QuickConfiguration = (IQuickConfiguration)GetDialogPage(typeof(ConfigurationPage));
 
             OverviewCommand.Initialize(this, dte, commandService);
             BuildService.Initialize(dte, eventManager);
         }
+
+        private async Task<T> GetServiceAsync<T>() => (T)await GetServiceAsync(typeof(T));
     }
 }
