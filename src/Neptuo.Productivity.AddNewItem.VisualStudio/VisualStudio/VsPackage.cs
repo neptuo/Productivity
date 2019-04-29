dd3ce249ce544ab70386f6018b2f6494fdc94acf
Marek Fišera <mara@neptuo.com>
2019-04-29 22:07:53 +0200

    #39 - Migrate to AsyncPackage.

--- src/Neptuo.Productivity.AddNewItem.VisualStudio/VisualStudio/VsPackage.cs
+++ src/Neptuo.Productivity.AddNewItem.VisualStudio/VisualStudio/VsPackage.cs
@@ -1,6 +1,6 @@
 ﻿using EnvDTE;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 using Neptuo.Productivity.VisualStudio.Commands;
 using Neptuo.Productivity.VisualStudio.Views;
 using System;
@@ -9,26 +9,37 @@ using System.ComponentModel.Design;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
 
 namespace Neptuo.Productivity.VisualStudio
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration(ProductInfo.Name, ProductInfo.Description, VersionInfo.Version, IconResourceID = 400)]
     [Guid(PackageGuids.PackageString)]
-    [ProvideAutoLoad(UIContextGuids80.NoSolution)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideToolWindow(typeof(AddNewItemWindow), Width = 400, Height = 200, Transient = true)]
-    public class VsPackage : Package
+    public class VsPackage : AsyncPackage
     {
-        protected override void Initialize()
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            base.Initialize();
+            await base.InitializeAsync(cancellationToken, progress);
+            await InitializeCommandsAsync(cancellationToken);
+        }
+
+        private async Task InitializeCommandsAsync(CancellationToken cancellationToken)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 
             XmlTemplateServiceFactory templateServiceFactory = new XmlTemplateServiceFactory();
 
-            IMenuCommandService commandService = (IMenuCommandService)GetService(typeof(IMenuCommandService));
+            DTE dte = await GetServiceAsync<DTE>();
+            IMenuCommandService commandService = await GetServiceAsync<IMenuCommandService>();
             AddNewItemCommand.Initialize(this, commandService);
         }
+
+        private async Task<T> GetServiceAsync<T>() => (T)await GetServiceAsync(typeof(T));
     }
 }
