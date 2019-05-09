fa7b66124121eeb31a9ddd2f328f692012ba7331
Marek Fišera <mara@neptuo.com>
2019-05-09 19:41:03 +0200

    TMP - Various examples of snippet inclusion.

--- src/Neptuo.Productivity.AddNewItem.VisualStudio/VisualStudio/Commands/AddNewItemCommand.cs
+++ src/Neptuo.Productivity.AddNewItem.VisualStudio/VisualStudio/Commands/AddNewItemCommand.cs
@@ -1,4 +1,4 @@
-﻿using EnvDTE;
+using EnvDTE;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Threading;
 using Neptuo.Collections.Specialized;
@@ -10,6 +10,11 @@ using System.IO;
 using System.Windows.Interop;
 using Window = System.Windows.Window;
 using Task = System.Threading.Tasks.Task;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Editor;
+using Neptuo;
+using Microsoft.VisualStudio;
 
 namespace Neptuo.Productivity.VisualStudio.Commands
 {
@@ -36,23 +41,101 @@ namespace Neptuo.Productivity.VisualStudio.Commands
 
         private async void OnExecuteAsync(object sender, EventArgs e)
         {
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-
             DTE dte = await package.GetServiceAsync<DTE>();
-            ITemplateService templates = await package.GetComponentServiceAsync<FirstNotNullTemplateService>();
-            IFileService files = new DteFileService(dte, package);
-            ICursorService cursor = new DteCursorService(dte);
 
-            var viewModel = new MainViewModel(files);
-            SetViewModelPath(dte, viewModel);
+            //string commandName = "Edit.InvokeSnippetFromShortcut";
+            //string commandArgs = "class";
+            //string commandName = "Edit.InsertSnippet";
+            //string commandArgs = "";
 
-            var wnd = new AddNewItemWindow(viewModel);
-            SetWindowOwner(dte, wnd);
+            try
+            {
+                dte.ActiveDocument.Activate();
 
-            if (wnd.ShowDialog() != true)
-                return;
+                //dte.ExecuteCommand(commandName, commandArgs);
 
-            await CreateItemAsync(templates, files, cursor, viewModel);
+                ((TextSelection)dte.ActiveDocument.Selection).Text = "class";
+                dte.ExecuteCommand("Edit.InsertTab");
+                dte.ExecuteCommand("Edit.InsertTab");
+
+                //dte.ExecuteCommand("Edit.InvokeSnippetFromShortcut");
+
+                //((TextSelection)dte.ActiveDocument.Selection).Text = "Ensure.NotNull($name$, \"$name$\");$end$";
+                //dte.ExecuteCommand("Edit.ShowSnippetHighlighting");
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+
+
+            try
+            {
+                DTE dte = await package.GetServiceAsync<DTE>();
+
+                IVsTextManager textManager = await package.GetServiceAsync<SVsTextManager, IVsTextManager>();
+                IVsTextManager2 textManager2 = (IVsTextManager2)textManager;
+
+                IVsTextView textView;
+                textManager.GetActiveView(1, null, out textView);
+
+                IVsEditorAdaptersFactoryService adapterFactory = await package.GetComponentServiceAsync<IVsEditorAdaptersFactoryService>();
+                IWpfTextView wpfTextView = adapterFactory.GetWpfTextView(textView);
+
+                IVsTextBuffer textBuffer = adapterFactory.GetBufferAdapter(wpfTextView.TextBuffer);
+                IVsExpansion expansion = (IVsExpansion)textBuffer;
+
+                textView.GetCaretPos(out int line, out int column);
+
+                var textSpan = new TextSpan()
+                {
+                    iStartLine = line,
+                    iStartIndex = column,
+                    iEndLine = line,
+                    iEndIndex = column
+                };
+
+                //https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.textmanager.interop.ivsexpansion?view=visualstudiosdk-2017
+                //https://github.com/dotnet/roslyn/blob/defeca5d0e44915fc978cfd93d3de2e4a6d0dbf4/src/VisualStudio/Core/Def/Implementation/Snippets/AbstractSnippetExpansionClient.cs
+                //https://github.com/adamdriscoll/poshtools/blob/ad99bb6ff6a284c39bf54611cc40a42b603f4277/PowerShellTools/Snippets/SnippetHandler.cs
+
+                IVsExpansionSession session;
+                Guid languageGuid = Guid.Parse("694dd9b6-b865-4c5b-ad85-86356e9c88dc");
+                //string title = "Ensure.NotNull";
+                //string path = @"D:\Development\Neptuo\Productivity\src\Neptuo.Productivity.CodeSnippets\CodeSnippets\CSharp\Neptuo\Neptuo.Ensure-NotNull.snippet";
+                string title = "attribute";
+                string path = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC#\Snippets\1033\Visual C#\attribute.snippet";
+                //expansion.InsertExpansion(textSpan, textSpan, null, languageGuid, out session);
+                //expansion.InsertSpecificExpansion(,,,,);
+                expansion.InsertNamedExpansion(title, path, textSpan, new VsExpansionClient(expansion, languageGuid, textSpan), languageGuid, 0, out session);
+
+
+                //IVsExpansionManager expansionManager;
+                //textManager2.GetExpansionManager(out expansionManager);
+                //expansionManager.InvokeInsertionUI(textView, new VsExpansionClient(expansion, languageGuid, textSpan), languageGuid, new string[] { "Expansion" }, 1, 0, null, 0, 0, "Expand: ", ".");
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+
+            //await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            //DTE dte = await package.GetServiceAsync<DTE>();
+            //ITemplateService templates = await package.GetComponentServiceAsync<FirstNotNullTemplateService>();
+            //IFileService files = new DteFileService(dte, package);
+            //ICursorService cursor = new DteCursorService(dte);
+
+            //var viewModel = new MainViewModel(files);
+            //SetViewModelPath(dte, viewModel);
+
+            //var wnd = new AddNewItemWindow(viewModel);
+            //SetWindowOwner(dte, wnd);
+
+            //if (wnd.ShowDialog() != true)
+            //    return;
+
+            //await CreateItemAsync(templates, files, cursor, viewModel);
         }
 
         private static void SetWindowOwner(DTE dte, AddNewItemWindow wnd)
